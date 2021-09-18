c32971143ea2e8072c724b66878a53d018954fa6
ivaan1992 <ivaan.lg.92@gmail.com>
2021-09-18 15:51:12 -0500

    Fix linters issues

--- about-script.js
+++ about-script.js
@@ -1,15 +1,15 @@
-const menu = document.querySelector(".menu-section")
-const closeHamburger = document.querySelector(".close")
+const menu = document.querySelector('.menu-section');
+const closeHamburger = document.querySelector('.close');
 const closeHamburgerEvent = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    if (menu.classList.contains("open")) {
-      menu.classList.remove("open");
-    } else {
-      menu.classList.add("open");
-    }
-}
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+  } else {
+    menu.classList.add('open');
+  }
+};
 
-closeHamburger.addEventListener("click", closeHamburgerEvent)
+closeHamburger.addEventListener('click', closeHamburgerEvent);
 
-document.querySelector("#hamburger-menu").addEventListener("click", closeHamburgerEvent);
+document.querySelector('#hamburger-menu').addEventListener('click', closeHamburgerEvent);
--- script.js
+++ script.js
@@ -1,43 +1,29 @@
-const menu = document.querySelector(".menu-section")
-const closeHamburger = document.querySelector(".close")
-const showMore = document.querySelector(".show-more")
+const menu = document.querySelector('.menu-section');
+const closeHamburger = document.querySelector('.close');
+const showMore = document.querySelector('.show-more');
 
 const closeHamburgerEvent = (e) => {
-    e.preventDefault()
+  e.preventDefault();
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+  } else {
+    menu.classList.add('open');
+  }
+};
 
-    if (menu.classList.contains("open")) {
-      menu.classList.remove("open");
-    } else {
-      menu.classList.add("open");
-    }
-}
+showMore.addEventListener('click', (e) => {
+  e.preventDefault();
+  const speakersContainer = document.querySelector('.speakers .cards-container');
+  if (!showMore.classList.contains('less')) {
+    showMore.classList.add('less');
+    speakersContainer.style.maxHeight = 'unset';
+    showMore.innerText = 'Show Less';
+  } else {
+    showMore.classList.remove('less');
+    speakersContainer.style = '';
+    showMore.innerText = 'Show More';
+  }
+});
+closeHamburger.addEventListener('click', closeHamburgerEvent);
 
-
-showMore.addEventListener("click", (e) => {
-    e.preventDefault()
-    const speakersContainer = document.querySelector(".speakers .cards-container")
-    if(!showMore.classList.contains("less")) {
-
-      showMore.classList.add("less")
-      speakersContainer.style.maxHeight = "unset"
-      showMore.innerText = "Show Less"
-      
-    } else {
-      showMore.classList.remove("less")
-      speakersContainer.style = ""
-      showMore.innerText = "Show More"
-    } 
-})
-closeHamburger.addEventListener("click", closeHamburgerEvent)
-
-document.querySelector("#hamburger-menu").addEventListener("click", closeHamburgerEvent);
-
-//links 
-
-const link = document.querySelectorAll('.link');
-
-link.forEach(
-  (link) => {
-    link.addEventListener('click', toggleMenu);
-  },
-);
+document.querySelector('#hamburger-menu').addEventListener('click', closeHamburgerEvent);
